0ce6da112e61bcd5699b271bce7d948a643c820b
xiao liu ya <63572603+xxscloud5722@users.noreply.github.com>
2026-01-12 16:50:58 +0800

    Update business-trip.sh

--- workspace/business-trip.sh
+++ workspace/business-trip.sh
@@ -1,1 +1,19 @@
-# 
+#!/bin/sh/env bash
+
+# 默认已安装 git
+
+
+mkdir -p ./billbear/business-trip
+cd ./xxscloud/barley-common-rock || {
+  echo "Error: Cannot cd to ./billbear/business-trip"
+  exit 1
+}
+
+git clone git@gitlab.liexiong.net:coral-interests/business-trip/coral-business-trip-biz.git
+git clone git@gitlab.liexiong.net:coral-interests/business-trip/coral-business-trip-datatask.git
+git clone git@gitlab.liexiong.net:coral-interests/business-trip/coral-business-trip-script.git
+
+cd - || {
+  echo "Error: Cannot cd -"
+  exit 1
+}
